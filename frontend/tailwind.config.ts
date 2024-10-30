486059d60166634a86142a87eb760a2b2f83d38d
Baah-Danso-Kenneth <kwadwobaahdanso@gmail.com>
2024-10-30 09:38:47 +0000

    Add images needed for the landing page - also removed default images which are prepopulated when installed nextjs in the public file.

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,16 +1,26 @@
 import type { Config } from "tailwindcss";
+const {fontFamily} = require('tailwindcss/defaultTheme');
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
+      fontFamily:{
+        pop:['var(--font-pop)',...fontFamily.sans],
+        nowy:['var(--font-nowy', ...fontFamily.sans]
+      },
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        dark:'#000000',
+        light:'#F2F2F4',
+        primary:'#A8FF8A',
+        darkGray:'#222222',
+        darkGrayHue:'#333333',
+        gray:'#969696',
+        grayTint:'#EFEFEF',
+        accent:'#B78BF2'
+    
       },
     },
   },
