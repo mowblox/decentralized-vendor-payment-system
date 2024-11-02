fa7839b286774dc0bfec69a6147ce1ac45ba7caa
Baah-Danso-Kenneth <kwadwobaahdanso@gmail.com>
2024-11-02 16:16:29 +0000

    Added functionality to route users to the dashboard screen when they click the "Get Started" button.

--- frontend/src/components/shared/Header.tsx
+++ frontend/src/components/shared/Header.tsx
@@ -4,6 +4,7 @@ import Image from 'next/image';
 import ThemeToggle from './ThemeToggle';
 import LightLogo from '../../../public/images/logo/logo1.png';
 import DarkLogo from '../../../public/images/logo/logo.png';
+import Link from 'next/link';
 
 interface HeaderProps {
   darkMode: boolean;
@@ -28,6 +29,7 @@ const Header: React.FC<HeaderProps> = ({ darkMode, toggleDarkMode }) => {
         </div>
         <div className='flex flex-row items-center gap-5'>
           <ThemeToggle darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
+          <Link href="/dashboard">
           <Button
             className={buttonVariants({
               size: "lg",
@@ -35,6 +37,7 @@ const Header: React.FC<HeaderProps> = ({ darkMode, toggleDarkMode }) => {
             })}>
             get started
           </Button>
+          </Link>
         </div>
       </div>
     </div>
