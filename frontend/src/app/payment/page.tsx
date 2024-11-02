07e524b094b54bd43e67f81fafaa67676d5a2f1d
Baah-Danso-Kenneth <kwadwobaahdanso@gmail.com>
2024-11-02 14:44:25 +0000

    Create layout for payment page

---
+++ frontend/src/app/payment/page.tsx
@@ -0,0 +1,11 @@
+
+import PaymentPage from '@/components/sections/paymentContent'
+import React from 'react'
+
+function Page() {
+  return (
+    <div><PaymentPage/></div>
+  )
+}
+
+export default Page
