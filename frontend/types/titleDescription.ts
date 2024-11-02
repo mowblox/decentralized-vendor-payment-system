37b0d8d9529167866021c473b48994b6a5040696
Baah-Danso-Kenneth <kwadwobaahdanso@gmail.com>
2024-11-02 14:46:05 +0000

    Add  types and color for payment component

---
+++ frontend/types/titleDescription.ts
@@ -0,0 +1,8 @@
+export interface TitleDescriptionProps{
+  title:string,
+  description:string,
+  className?:React.ReactNode, 
+  titleStyle?:React.ReactNode, 
+  descriptionStyle?:React.ReactNode
+}
+
