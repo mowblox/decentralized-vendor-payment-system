8abdca5e2a46b816921a5d4e48ccf6d3ce2e4ab9
Baah-Danso-Kenneth <kwadwobaahdanso@gmail.com>
2024-11-02 16:14:06 +0000

    Added new pages and defined components to ensure a structured layout that fits well within the dashboard's user interface

---
+++ frontend/src/app/dashboard/createLink/page.tsx
@@ -0,0 +1,6 @@
+import CreateLink from "@/components/sections/dashboard/CreateLink";
+
+
+export default function Page() {
+  return <><CreateLink/></>;
+}
---
+++ frontend/src/components/sections/dashboard/EmptyLink.tsx
@@ -0,0 +1,21 @@
+
+import Link from 'next/link'
+import React from 'react'
+import UserInformation from './UserInformation'
+
+function EmptyLinks() {
+  return (
+    <div>
+      <div className='flex items-end justify-end'>
+        <UserInformation/>
+      </div>
+
+      <div>
+        <h1 className='font-nowy text-4xl capitalize mb-3 dark:text-light'>dashboard</h1>
+        <p className='font-pop dark:text-light'>you have no links yet, <span className='dark:text-primary text-blue-400 underline'><Link href="/dashboard/createLink/">click here to create one instantly</Link></span></p>
+      </div>
+      </div>
+  )
+}
+
+export default EmptyLinks
